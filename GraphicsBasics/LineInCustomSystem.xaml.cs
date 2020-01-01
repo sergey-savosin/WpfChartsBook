d9be75243e90ed7b7164138507f9f26ce757f5d3
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2020-01-01 12:09:01 +0300

    Custom system

---
+++ GraphicsBasics/LineInCustomSystem.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sergey
+ * Date: 21.12.2019
+ * Time: 22:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace GraphicsBasics
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class LineInCustomSystem : Window
+	{
+		public LineInCustomSystem()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ GraphicsBasics/ScaleInCustomSystem.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sergey
+ * Date: 29.12.2019
+ * Time: 22:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace GraphicsBasics
+{
+	/// <summary>
+	/// Interaction logic for ScaleInCustomSystem.xaml
+	/// </summary>
+	public partial class ScaleInCustomSystem : Window
+	{
+		public ScaleInCustomSystem()
+		{
+			InitializeComponent();
+		}
+	}
+}
